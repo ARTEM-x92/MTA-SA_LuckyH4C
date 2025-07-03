c2ccdd111ebdf43fa2591b3f792cb13b5e05a293
Unknown User <115232312+IAmUnknownUser-0x00-0xFF@users.noreply.github.com>
2025-07-03 21:55:21 +0400

    LScript_its-BATch.cmd

---
+++ LScript_its-BATch.cmd
@@ -0,0 +1,27 @@
+rem @ECHO OFF
+rem REM C:\Windows\cmd.exe
+rem COLOR 0A
+rem TITLE "MT-AUTO SA LuckyH4C"
+
+rem REM H2O + U Insyde BATCH
+
+rem REM x92 Bit (impossible is possible)
+
+rem ECHO.  
+rem ECHO "Antarctican, Cold Start Script for Lazy Humans MT-AUTO SA LuckyH4C By Alfa 92, Script Accepted by 5333, 0xFF"
+REM ECHO Non User Friendly Version
+rem ECHO.  
+
+# # Script Vars [Do Not Change] 
+#Dir="~/MTA-SA_LuckyH4C/"
+#Assets_Dir=$Dir"Assets/"
+############./$DIRmain.cfg
+
+rem REM Script Vars
+rem REM Do Not Change
+rem SET Dir=
+rem SET Assets_Dir=
+
+rem SET DEBUG=0
+rem SET AUTO=0
+
