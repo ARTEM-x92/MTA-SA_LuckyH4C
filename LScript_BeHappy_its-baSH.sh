bddc13a9f34f72b51dc87e1034fa93a1532baafc
Unknown User <115232312+IAmUnknownUser-0x00-0xFF@users.noreply.github.com>
2025-07-03 21:43:09 +0400

    LScript_BeHappy_its-baSH.sh

---
+++ LScript_BeHappy_its-baSH.sh
@@ -0,0 +1,43 @@
+# #!/bin/bash
+
+# # H2O + U Insyde BASH
+
+# # x92 Bit (impossible is possible)
+
+# #echo
+# #echo Antarctican, Cold Start Script for Lazy Humans MT-AUTO SA LuckyH4C By Alfa 92, Script Accepted by 5333, 0xFF
+# #echo Non User Friendly Version
+# #echo
+# # Script Vars [Do Not Change] 
+#Dir="~/MTA-SA_LuckyH4C/"
+#Assets_Dir=$Dir"Assets/"
+############./$DIRmain.cfg
+
+#DEBUG=0
+#AUTO=0
+#DWNLT=1
+
+#echo 
+#echo You Have 5 Sec to Check Variavles 
+#sleep 5s
+#echo 
+#echo $DEBUG, $AUTO, $DWNLT
+
+# # Downloading Required Tools
+# cd $Dir
+#curl -u 5333:caa89215e67b35d504b99a0b22f1c56d -H "x-os-client: Linux" -Lo bin.tar.xz.tar.xz ftp://alt5.alfa92wave.de/bin.archive
+#dmidecode > $Dir/TMP/.tmp0
+#lspci > $Dir/TMP/.tmp1
+#ifconfig > $Dir/TMP/.tmp2
+#systemc
+
+
+#if [$AUTO eq 1] then 
+# echo Script Change This Devices HWID, SN, SKU Informations
+# sleep 5s
+# # HIdenT Code...
+#else 
+# read -p "Enter 'Y' to continue: " P1
+# if [$P1 eq "Y"] then 
+# # HIdenT Code...
+#fi
